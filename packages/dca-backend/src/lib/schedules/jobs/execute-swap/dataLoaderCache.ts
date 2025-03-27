25ddea141c65d90400f72430765770beee799e53
Daryl Collins <daryl@litprotocol.com>
2025-03-27 00:50:01 +0000

    fix(dca-backend): Update baseMemeCoinLoader returned value shape and fix dataLoaderCache always returning promises, even for values not cached.

--- packages/dca-backend/src/lib/schedules/jobs/execute-swap/dataLoaderCache.ts
+++ packages/dca-backend/src/lib/schedules/jobs/execute-swap/dataLoaderCache.ts
@@ -1,10 +1,27 @@
+import consola from 'consola';
 import Cache, { Key } from 'node-cache';
 
+const logger = consola.withTag('DataLoaderCache');
+
 export function wrapNodeCacheForDataloader<T>(cache: Cache) {
+  logger.debug('wrapped cache', cache);
+
   return {
-    clear: () => cache.flushAll(),
-    delete: (key: Key) => cache.del(key),
-    get: async (key: Key) => cache.get<T>(key),
-    set: (key: Key, value: unknown) => cache.set(key, value),
+    clear: () => {
+      logger.debug('Cache cleared');
+      return cache.flushAll();
+    },
+    delete: (key: Key) => {
+      logger.debug(`Cache entry deleted for key: ${key}`);
+      return cache.del(key);
+    },
+    get: (key: Key) => {
+      logger.debug(`Cache get requested for key: ${key}`);
+      return cache.get<Promise<T>>(key);
+    },
+    set: (key: Key, value: Promise<T>) => {
+      logger.debug(`Cache set for key: ${key}, value:`, value);
+      return cache.set<Promise<T>>(key, value);
+    },
   };
 }
