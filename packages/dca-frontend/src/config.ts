53f9b3008998f4b5edf0f7676b1108bd8e667919
FedericoAmura <federicoamura@gmail.com>
2025-03-27 15:21:21 +0100

    feat: use updated consent page url format

--- packages/dca-frontend/src/config.ts
+++ packages/dca-frontend/src/config.ts
@@ -4,5 +4,5 @@ export const APP_ID = import.meta.env.VITE_APP_ID;
 export const APP_VERSION = import.meta.env.VITE_APP_VERSION;
 export const REDIRECT_URI = import.meta.env.VITE_REDIRECT_URI || window.location.origin;
 export const CONSENT_PAGE_BASE =
-  import.meta.env.VITE_CONSENT_PAGE_BASE_URL || `https://dashboard.heyvincent.ai/consent`;
-export const CONSENT_PAGE_DCA_URL = `${CONSENT_PAGE_BASE}/${APP_ID}?redirectUri=${REDIRECT_URI}`;
+  import.meta.env.VITE_CONSENT_PAGE_BASE_URL || `https://dashboard.heyvincent.ai`;
+export const CONSENT_PAGE_DCA_URL = `${CONSENT_PAGE_BASE}/${APP_ID}/consent?redirectUri=${REDIRECT_URI}`;
