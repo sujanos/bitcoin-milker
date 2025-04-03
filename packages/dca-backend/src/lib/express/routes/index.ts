e4b535b3090cbe4d2e3020799c915da851c83c28
Daryl Collins <daryl@litprotocol.com>
2025-04-03 17:59:49 +0100

    fix(dca-backend): Simplify CORS configuration

--- packages/dca-backend/src/lib/express/routes/index.ts
+++ packages/dca-backend/src/lib/express/routes/index.ts
@@ -18,28 +18,7 @@ const { CORS_ALLOWED_DOMAIN, IS_DEVELOPMENT } = env;
 
 const corsConfig = {
   optionsSuccessStatus: 204,
-  origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
-    if (!origin) {
-      callback(new Error('Not allowed by CORS'));
-      return;
-    }
-
-    const allowedOrigins = [
-      // eslint-disable-next-line no-useless-escape
-      new RegExp(`^https?:\/\/${CORS_ALLOWED_DOMAIN}$`),
-    ];
-
-    if (IS_DEVELOPMENT) {
-      // localhost with any port only allowed when not running in production
-      allowedOrigins.push(/^https?:\/\/localhost(:\d+)?$/);
-    }
-
-    if (allowedOrigins.some((regex) => regex.test(origin))) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: IS_DEVELOPMENT ? true : [CORS_ALLOWED_DOMAIN],
 };
 
 export const registerRoutes = (app: Express) => {
@@ -49,8 +28,8 @@ export const registerRoutes = (app: Express) => {
     serviceLogger.info(`CORS is disabled for development`);
   } else {
     serviceLogger.info(`Configuring CORS with allowed domain: ${CORS_ALLOWED_DOMAIN}`);
-    app.use(cors(corsConfig));
   }
+  app.use(cors(corsConfig));
 
   app.get('/purchases', authenticateUser, handleListPurchasesRoute);
   app.get('/schedules', authenticateUser, handleListSchedulesRoute);
