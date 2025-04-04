456f0dbecb85ddd68f86929d55425126d29f4d73
Daryl Collins <daryl@litprotocol.com>
2025-04-05 00:26:12 +0100

    feat(dca-frontend): Add a spinner and 'you have no active DCAs' state to `active-dcas.tsx`

--- packages/dca-frontend/src/components/active-dcas.tsx
+++ packages/dca-frontend/src/components/active-dcas.tsx
@@ -16,6 +16,97 @@ import {
 } from '@/components/ui/table';
 import { DialogueEditDCA } from '@/components/dialogue-edit-dca';
 import { FREQUENCIES } from '@/components/select-frequency';
+import { Spinner } from '@/components/ui/spinner';
+
+function renderDCASchedulesTable(
+  activeDCAs: DCA[],
+  handleUpdatedDCA: (updatedDCA: DCA) => Promise<void>,
+  handleDisableDCA: (dcaId: string) => Promise<void>,
+  handleEnableDCA: (dcaId: string) => Promise<void>,
+  handleDeleteDCA: (dcaId: string) => Promise<void>
+) {
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Amount (USD)</TableHead>
+          <TableHead>Frequency</TableHead>
+          <TableHead>Last Update</TableHead>
+          <TableHead>Status</TableHead>
+          <TableHead className="text-right">Actions</TableHead>
+        </TableRow>
+      </TableHeader>
+
+      <TableBody>
+        {activeDCAs.map((dca) => {
+          const uniqueKey = dca._id;
+
+          return (
+            <TableRow key={uniqueKey}>
+              <TableCell>${dca.purchaseAmount}</TableCell>
+              <TableCell>
+                {FREQUENCIES.find((freq) => freq.value === dca.purchaseIntervalHuman)?.label ||
+                  dca.purchaseIntervalHuman}
+              </TableCell>
+              <TableCell>{new Date(dca.updatedAt).toLocaleString()}</TableCell>
+              <TableCell>
+                <span>{dca.active ? 'Active' : 'Inactive'}</span>
+              </TableCell>
+              <TableCell>
+                <Box className="flex flex-row items-center justify-end gap-2 p-1">
+                  <DialogueEditDCA dca={dca} onUpdate={handleUpdatedDCA} />
+                  {dca.active ? (
+                    <Button variant="destructive" onClick={() => handleDisableDCA(dca._id)}>
+                      <Pause />
+                    </Button>
+                  ) : (
+                    <Button variant="default" onClick={() => handleEnableDCA(dca._id)}>
+                      <Play />
+                    </Button>
+                  )}
+                  <Button variant="destructive" onClick={() => handleDeleteDCA(dca._id)}>
+                    <Delete />
+                  </Button>
+                </Box>
+              </TableCell>
+            </TableRow>
+          );
+        })}
+      </TableBody>
+    </Table>
+  );
+}
+
+function renderSpinner() {
+  return (
+    <div className="flex items-center justify-center">
+      <Spinner />
+    </div>
+  );
+}
+
+function renderContent(
+  activeDCAs: DCA[],
+  isLoading: boolean,
+  handleUpdatedDCA: (updatedDCA: DCA) => Promise<void>,
+  handleDisableDCA: (dcaId: string) => Promise<void>,
+  handleEnableDCA: (dcaId: string) => Promise<void>,
+  handleDeleteDCA: (dcaId: string) => Promise<void>
+) {
+  if (!activeDCAs.length && isLoading) {
+    return renderSpinner();
+  } else if (activeDCAs.length) {
+    return renderDCASchedulesTable(
+      activeDCAs,
+      handleUpdatedDCA,
+      handleDisableDCA,
+      handleEnableDCA,
+      handleDeleteDCA
+    );
+  } else {
+    return <div className="flex justify-center">No active DCAs</div>;
+  }
+}
 
 export const ActiveDcas: React.FC = () => {
   const [activeDCAs, setActiveDCAs] = useState<DCA[]>([]);
@@ -26,9 +117,20 @@ export const ActiveDcas: React.FC = () => {
   useEffect(() => {
     const fetchDCAs = async () => {
       try {
+        const fetchDelay = 500;
+        const startTime = Date.now();
+
         const dcas = await getDCAs();
+        const elapsedTime = Date.now() - startTime;
+
         setActiveDCAs(dcas);
-        setIsLoading(false);
+
+        const remainingTime = fetchDelay - elapsedTime;
+        if (remainingTime > 0) {
+          setTimeout(() => setIsLoading(false), remainingTime);
+        } else {
+          setIsLoading(false);
+        }
       } catch (error) {
         console.error('Error fetching active DCAs:', error);
         setIsLoading(false);
@@ -97,15 +199,6 @@ export const ActiveDcas: React.FC = () => {
     [activeDCAs, deleteDCA, setActiveDCAs]
   );
 
-  if (!activeDCAs.length && isLoading) {
-    // TODO add a loading indicator while fetching, then show table even if empty
-    return (
-      <div className="dcas-list">
-        <div className="loading-indicator">Loading dcas...</div>
-      </div>
-    );
-  }
-
   return (
     <Card data-test-id="active-dcas" className="w-full bg-white p-6 shadow-md">
       <CardHeader className="text-center">
@@ -115,54 +208,14 @@ export const ActiveDcas: React.FC = () => {
       <Separator />
 
       <CardContent>
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead>Amount (USD)</TableHead>
-              <TableHead>Frequency</TableHead>
-              <TableHead>Last Update</TableHead>
-              <TableHead>Status</TableHead>
-              <TableHead className="text-right">Actions</TableHead>
-            </TableRow>
-          </TableHeader>
-
-          <TableBody>
-            {activeDCAs.map((dca) => {
-              const uniqueKey = dca._id;
-
-              return (
-                <TableRow key={uniqueKey}>
-                  <TableCell>${dca.purchaseAmount}</TableCell>
-                  <TableCell>
-                    {FREQUENCIES.find((freq) => freq.value === dca.purchaseIntervalHuman)?.label ||
-                      dca.purchaseIntervalHuman}
-                  </TableCell>
-                  <TableCell>{new Date(dca.updatedAt).toLocaleString()}</TableCell>
-                  <TableCell>
-                    <span>{dca.active ? 'Active' : 'Inactive'}</span>
-                  </TableCell>
-                  <TableCell>
-                    <Box className="flex flex-row items-center justify-end gap-2 p-1">
-                      <DialogueEditDCA dca={dca} onUpdate={handleUpdatedDCA} />
-                      {dca.active ? (
-                        <Button variant="destructive" onClick={() => handleDisableDCA(dca._id)}>
-                          <Pause />
-                        </Button>
-                      ) : (
-                        <Button variant="default" onClick={() => handleEnableDCA(dca._id)}>
-                          <Play />
-                        </Button>
-                      )}
-                      <Button variant="destructive" onClick={() => handleDeleteDCA(dca._id)}>
-                        <Delete />
-                      </Button>
-                    </Box>
-                  </TableCell>
-                </TableRow>
-              );
-            })}
-          </TableBody>
-        </Table>
+        {renderContent(
+          activeDCAs,
+          isLoading,
+          handleUpdatedDCA,
+          handleDisableDCA,
+          handleEnableDCA,
+          handleDeleteDCA
+        )}
       </CardContent>
     </Card>
   );
