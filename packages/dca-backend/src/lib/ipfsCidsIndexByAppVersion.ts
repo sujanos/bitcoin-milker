9ca4a135d34681543554cfd35e4725c39f4ebff8
FedericoAmura <federicoamura@gmail.com>
2025-04-17 12:14:38 +0200

    feat: add ipfs cids for dca prod app

--- packages/dca-backend/src/lib/ipfsCidsIndexByAppVersion.ts
+++ packages/dca-backend/src/lib/ipfsCidsIndexByAppVersion.ts
@@ -1,5 +1,10 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 export const IPFS_CIDS_INDEX_BY_APP_VERSION = {
+  1: {
+    ERC20_APPROVAL_TOOL: 'QmPZ46EiurxMb7DmE9McFyzHfg2B6ZGEERui2tnNNX7cky',
+    SPENDING_LIMIT_POLICY: 'QmZrG2DFvVDgo3hZgpUn31TUgrHYfLQA2qEpAo3tnKmzhQ',
+    UNISWAP_SWAP_TOOL: 'QmZbh52JYnutuFURnpwfywfiiHuFoJpqFyFzNiMtbiDNkK',
+  },
   11: {
     ERC20_APPROVAL_TOOL: 'QmPZ46EiurxMb7DmE9McFyzHfg2B6ZGEERui2tnNNX7cky',
     SPENDING_LIMIT_POLICY: 'QmZrG2DFvVDgo3hZgpUn31TUgrHYfLQA2qEpAo3tnKmzhQ',
